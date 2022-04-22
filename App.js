4216b15cf6d081cd8ca3bcecff03efc11fb1b87b
GILANG RIZKY RAMADHAN <83531327+GilangRizkyRamadhan119140081@users.noreply.github.com>
2022-04-22 16:22:38 +0700

    Delete App.js

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { View, Text} from 'react-native'
-import Navigation from './src/navigation/navigation'
-
-export default function App() {
-    return (
-        <Navigation/>
-    );
-}
